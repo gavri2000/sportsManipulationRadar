8e675a7c1fb5874933ddbf99ad12596c9a9b1f3f
Orr Margaliot <ormargol@gmail.com>
2017-08-27 15:25:06 +0300

    RR: add algorithm code (version 1 - bad efficiency)
    Signed-off-by: Orr Margaliot <ormargol@gmail.com>

--- src/com/eran/RoundRobinAlgorithm.java
+++ src/com/eran/RoundRobinAlgorithm.java
@@ -24,34 +24,43 @@ public class RoundRobinAlgorithm {
         List<Pair<Team,Team>> pairList = new ArrayList<>();
         Group groupOfTeam = getGroupOfTeam(team, listOfGroups);
         groupOfTeam.SortTeams();
-
-        //TODO:  instead of the following if, we need to find if there was manipulates
-        /*
-            for (int t1 = 0; t1 <= 1; t1++) {
-                for (int t2 = 0; t2 <=1 ; t2++) {
-                    for (int t3 = 0; t3 <=1 ; t3++) {
-                        for (int t4 = 0; t4 <=1 ; t4++) {
-                            if (t1 == 1 && teams[0] is in coalision && teams[0] is not "our team" && teams[0] is better than "our team")
-                            {
-                                teams[0].score -= 3;
-                                "our team".score += 3;
-                            }
-                            if (t2 == 1 && teams[1] is in coalision && teams[1] is not "our team" && teams[1] is better than "our team")
-                            {
-                                teams[1].score -= 3;
-                                "our team".score += 3;
-                            }
-                            ...
-                            teams.sort();
-                            if winnersPair dont contains the pair <teams[0], teams[1]> && "our team" is inside that pair, add it.
+        Team tm1 = groupOfTeam.getTeams().get(0);
+        Team tm2 = groupOfTeam.getTeams().get(1);
+        Team tm3 = groupOfTeam.getTeams().get(2);
+        Team tm4 = groupOfTeam.getTeams().get(3);
+        for (int t1 = 0; t1 <= 1; t1++) {
+            for (int t2 = 0; t2 <=1 ; t2++) {
+                for (int t3 = 0; t3 <=1 ; t3++) {
+                    for (int t4 = 0; t4 <=1 ; t4++) {
+                        if (t1 == 1 && coalition.contains(tm1) && tm1 != team && tournamentGraph[tm1.getTeamNumber()-1][team.getTeamNumber()])
+                        {
+                            tm1.setScore(tm1.getScore() - 3);
+                            team.setScore(team.getScore() + 3);
                         }
+                        if (t2 == 1 && coalition.contains(tm2) && tm2 != team && tournamentGraph[tm2.getTeamNumber()-1][team.getTeamNumber()])
+                        {
+                            tm2.setScore(tm2.getScore() - 3);
+                            team.setScore(team.getScore() + 3);
+                        }
+                        if (t3 == 1 && coalition.contains(tm3) && tm1 != team && tournamentGraph[tm3.getTeamNumber()-1][team.getTeamNumber()])
+                        {
+                            tm3.setScore(tm3.getScore() - 3);
+                            team.setScore(team.getScore() + 3);
+                        }
+                        if (t4 == 1 && coalition.contains(tm4) && tm4 != team && tournamentGraph[tm4.getTeamNumber()-1][team.getTeamNumber()])
+                        {
+                            tm4.setScore(tm4.getScore() - 3);
+                            team.setScore(team.getScore() + 3);
+                        }
+                        groupOfTeam.getTeams().sort();
+                        if ((team == groupOfTeam.getTeams().get(0) || team == groupOfTeam.getTeams().get(1)) && !pairsList.contains(new Pair(groupOfTeam.getTeams().get(0), groupOfTeam.getTeams().get(1))) {
+                            pairsList.add(new Pair(groupOfTeam.getTeams().get(0), groupOfTeam.getTeams().get(1)));
+                            System.out.println(groupOfTeam.getTeams().get(0).getNumber() + "," + groupOfTeam.getTeams().get(1).getNumber());
+                        }
+                        initListOfGroupsScores(tournamentGraph, listOfGroups);
                     }
                 }
             }
-         */
-        //the following if is a private case of the previous comment, and will be removed after the prev comment will be coded
-        if (groupOfTeam.getTeams().get(0) == team || groupOfTeam.getTeams().get(1) == team) {
-            pairList.add(new Pair<>(groupOfTeam.getTeams().get(0), groupOfTeam.getTeams().get(1)));
         }
         return pairList;
     }
@@ -67,9 +76,11 @@ public class RoundRobinAlgorithm {
         return null;
     }
 
-
     public void initListOfGroupsScores(int[][] tournamentGraph, List<Group> listOfGroups) {
         for (int i = 0; i < NUM_OF_GROUPS; i++) {
+            for (int j = 0; j < NUM_OF_TEAMS; j++) {
+                listOfGroups.get(i).getTeams().get(j).setScore(0);
+            }
             for (int j = 0; j < NUM_OF_TEAMS; j++) {
                 for (int k = j + 1; k < NUM_OF_TEAMS; k++) {
                     Team team1 = listOfGroups.get(i).getTeams().get(j);
