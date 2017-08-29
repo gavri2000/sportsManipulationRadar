48a548ca8a1dcb7aa6aed9de3f5fcb63817b3272
gavri2000 <gavri2000@gmail.com>
2017-08-29 23:13:18 +0300

    fix method possibleManipulatedWinnerPairs

--- src/com/eran/RoundRobinAlgorithm.java
+++ src/com/eran/RoundRobinAlgorithm.java
@@ -16,11 +16,11 @@ public class RoundRobinAlgorithm {
 
         // fill the scores array by the strengths from tournament graph
         initListOfGroupsScores(tournamentGraph, listOfGroups);
-        List<Pair<Team,Team>> pairList = possibleManipulatedWinnerPairs(team, tournamentGraph, listOfGroups);
+        List<Pair<Team,Team>> pairList = possibleManipulatedWinnerPairs(team, tournamentGraph, listOfGroups, coalition);
         return listOfPossibleWinners;
     }
 
-    private List<Pair<Team,Team>> possibleManipulatedWinnerPairs(Team team, int[][] tournamentGraph, List<Group> listOfGroups) {
+    private List<Pair<Team,Team>> possibleManipulatedWinnerPairs(Team team, int[][] tournamentGraph, List<Group> listOfGroups, List<Team> coalition) {
         List<Pair<Team,Team>> pairList = new ArrayList<>();
         Group groupOfTeam = getGroupOfTeam(team, listOfGroups);
         groupOfTeam.SortTeams();
@@ -32,30 +32,27 @@ public class RoundRobinAlgorithm {
             for (int t2 = 0; t2 <=1 ; t2++) {
                 for (int t3 = 0; t3 <=1 ; t3++) {
                     for (int t4 = 0; t4 <=1 ; t4++) {
-                        if (t1 == 1 && coalition.contains(tm1) && tm1 != team && tournamentGraph[tm1.getTeamNumber()-1][team.getTeamNumber()])
-                        {
+                        // TODO: fix the last condition of all the if commands (tournamentGraph[tm#.getTeamNumber() - 1][team.getTeamNumber()] == 000)
+                        if ((t1 == 1) && (coalition.contains(tm1)) && (tm1 != team) && (tournamentGraph[tm1.getTeamNumber() - 1][team.getTeamNumber()] == 000)) {
                             tm1.setScore(tm1.getScore() - 3);
                             team.setScore(team.getScore() + 3);
                         }
-                        if (t2 == 1 && coalition.contains(tm2) && tm2 != team && tournamentGraph[tm2.getTeamNumber()-1][team.getTeamNumber()])
-                        {
+                        if ((t2 == 1) && (coalition.contains(tm2)) && (tm2 != team) && (tournamentGraph[tm2.getTeamNumber() - 1][team.getTeamNumber()] == 000)) {
                             tm2.setScore(tm2.getScore() - 3);
                             team.setScore(team.getScore() + 3);
                         }
-                        if (t3 == 1 && coalition.contains(tm3) && tm1 != team && tournamentGraph[tm3.getTeamNumber()-1][team.getTeamNumber()])
-                        {
+                        if ((t3 == 1) && (coalition.contains(tm3)) && (tm1 != team) && (tournamentGraph[tm3.getTeamNumber() - 1][team.getTeamNumber()] == 000)) {
                             tm3.setScore(tm3.getScore() - 3);
                             team.setScore(team.getScore() + 3);
                         }
-                        if (t4 == 1 && coalition.contains(tm4) && tm4 != team && tournamentGraph[tm4.getTeamNumber()-1][team.getTeamNumber()])
-                        {
+                        if ((t4 == 1) && (coalition.contains(tm4)) && (tm4 != team) && (tournamentGraph[tm4.getTeamNumber() - 1][team.getTeamNumber()] == 000)) {
                             tm4.setScore(tm4.getScore() - 3);
                             team.setScore(team.getScore() + 3);
                         }
-                        groupOfTeam.getTeams().sort();
-                        if ((team == groupOfTeam.getTeams().get(0) || team == groupOfTeam.getTeams().get(1)) && !pairsList.contains(new Pair(groupOfTeam.getTeams().get(0), groupOfTeam.getTeams().get(1))) {
-                            pairsList.add(new Pair(groupOfTeam.getTeams().get(0), groupOfTeam.getTeams().get(1)));
-                            System.out.println(groupOfTeam.getTeams().get(0).getNumber() + "," + groupOfTeam.getTeams().get(1).getNumber());
+                        groupOfTeam.SortTeams();
+                        if ((team == groupOfTeam.getTeams().get(0) || team == groupOfTeam.getTeams().get(1)) && !pairList.contains(new Pair(groupOfTeam.getTeams().get(0), groupOfTeam.getTeams().get(1)))) {
+                            pairList.add(new Pair(groupOfTeam.getTeams().get(0), groupOfTeam.getTeams().get(1)));
+                            System.out.println(groupOfTeam.getTeams().get(0).getTeamNumber() + "," + groupOfTeam.getTeams().get(1).getTeamNumber());
                         }
                         initListOfGroupsScores(tournamentGraph, listOfGroups);
                     }
